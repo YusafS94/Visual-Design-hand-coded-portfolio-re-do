08405dfa796cf00c3a980a66b8dfca01e089242a
Yusaf <yusafsaddiq@gmail.com>
2023-10-13 12:42:05 +0100

    pre-redo mobile first

--- script.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("navOverlay").style.width = "100%";
-    document.getElementById("openNav").style.display = "none";
-}
-
-function closeNav() {
-    document.getElementById("navOverlay").style.width = "0%";
-    document.getElementById("openNav").style.display = "block";
-}
