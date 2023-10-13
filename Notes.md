304d17ea7b476b1ee9629118a5321ecf10644af8
Yusaf <yusafsaddiq@gmail.com>
2023-10-13 11:35:37 +0100

    first commit. header and animated skills bar currently added

---
+++ Notes.md
@@ -0,0 +1,9 @@
+# Notes
+
+## Design files:
+* "./Design files/Desktop View.png"
+
+## Techniques and tutorials needed
+* Auto scrolling section: https://www.youtube.com/watch?v=iCl9aVo4xkI (Start 46:44)
+* Animation tutorial starts 50:45, ends at 54:06
+
---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navOverlay").style.width = "100%";
+    document.getElementById("openNav").style.display = "none";
+}
+
+function closeNav() {
+    document.getElementById("navOverlay").style.width = "0%";
+    document.getElementById("openNav").style.display = "block";
+}
